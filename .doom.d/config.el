69e8dfe834d4b326766fad3cbc321c108644a2cc
Vincent Stollenwerk <vstollen@gmail.com>
2021-07-25 15:44:41 +0200

    Move org-roam folder

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -29,7 +29,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Dokumente/Notes/org/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -53,7 +53,7 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(setq org-roam-directory "~/Dokumente/org-roam")
+(setq org-roam-directory "~/Dokumente/Notes/org-roam")
 (setq org-roam-encrypt-files t)
 
 (setq doom-font (font-spec :family "JetBrains Mono" :size 16))
@@ -63,4 +63,4 @@
   :config
   (require 'org-ref))
 
-(add-to-list 'auto-mode-alist '("\\.liquid'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.liquid\\'" . web-mode))
