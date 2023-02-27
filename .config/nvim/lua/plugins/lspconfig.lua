096a2025d134a2581cf4e4478caf994904d3aba3
Vincent Stollenwerk <hello@vstollen.me>
2023-02-27 23:33:52 +0100

    Add mappings for lsp

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,1 +1,9 @@
-require('lspconfig').gopls.setup{}
+require('lspconfig').gopls.setup{
+	on_attach = function()
+		vim.keymap.set({ 'i', 'n' } , '<c-space>', vim.lsp.buf.hover, {buffer=0})
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {buffer=0})
+		vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, {buffer=0})
+		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {buffer=0})
+		vim.keymap.set('n', 'gr', vim.lsp.buf.references, {buffer=0})
+	end
+}
