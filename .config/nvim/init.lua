f52e6367afedd49f28f65c95eeeb7f15767a7005
Vincent Stollenwerk <hello@vstollen.me>
2024-11-30 18:46:57 +0100

    Change default nvim tab width to 4

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,9 +6,9 @@ vim.o.backup = false
 vim.o.undodir = os.getenv('HOME') .. '/.vim/undodir'
 vim.o.undofile = true
 
-vim.o.tabstop = 2
-vim.o.softtabstop = 2
-vim.o.shiftwidth = 2
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
 vim.o.expandtab = true
 
 vim.o.number = true
